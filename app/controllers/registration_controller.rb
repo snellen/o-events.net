cd748336dbec82da2da1e415b44f5a3466786421
Martin Zoller <git@spam.zoller.tv>
2011-01-28 01:12:38 -0500

    Edited registration controller & views, added partials

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationController < ApplicationController
   def overview
     @event = Event.find(params[:event_id])
+    @title = @event.name
     @competition_groups = CompetitionGroup.find(Event.find(params[:event_id]).competitions.map(&:competition_group_id))
     if @competition_groups.count == 1
       redirect_to :action => 'main', :competition_group_id => @competition_groups[0].id
@@ -8,17 +9,31 @@ class RegistrationController < ApplicationController
   end
 
   def main
-    @teams = CompetitionGroup.find(params[:competition_group_id]).teams
+    @competition_group = CompetitionGroup.find(params[:competition_group_id])
+    @teams = @competition_group.teams
+    # The following will raise an exception if a CompetitionGroup has no Competitions - but that shouldn't happen at this stage.
+    @event = @competition_group.competitions.first.event
+    @title = @event.name
     if @teams.empty?
       redirect_to :action => 'team_members', :competition_group_id => params[:competition_group_id] 
     end
   end
 
   def team_members
-    if params[:team_id]
+    @competition_group = CompetitionGroup.find(params[:competition_group_id])
+    @event = @competition_group.competitions.first.event
+    @title = @event.name
+
+    if params[:team_member_id]
+      @team_member = TeamMember.find(params[:team_member_id])
+      @team_members = @team_member.team.team_members
+    elsif params[:team_id]
       @team_members = Team.find(params[:team_id]).team_members
+      @team_member = TeamMember.new
     else
       @team_members = []
+      @team_member = TeamMember.new
+    end
   end
 
   def team_options
