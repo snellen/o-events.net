f3b314d1c3509f7abfdff0078f87aaa6eb21fee5
Silvan Nellen <silvan.nellen@gmail.com>
2011-01-29 22:11:19 -0500

    begin implementation of validations

--- app/models/age_range.rb
+++ app/models/age_range.rb
@@ -1,5 +1,8 @@
 class AgeRange < ActiveRecord::Base
   belongs_to :event
+  validates_presence_of :event
   has_many :start_fees
   has_many :additional_fees
+  validates_numericality_of :lower, :less_than => :higher, :greater_than => 0, :only_integer => true
+  validates_numericality_of :higher, :less_than => 100, :greater_than => :lower, :only_integer => true
 end
--- app/models/category_group.rb
+++ app/models/category_group.rb
@@ -1,4 +1,5 @@
 class CategoryGroup < ActiveRecord::Base
   has_many :categories
   has_many :start_fees
+  validates_presence_of :fee_age_dependent
 end
