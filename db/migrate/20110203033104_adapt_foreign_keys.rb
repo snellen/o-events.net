563800115e7419b7f10bc4e5a190d550a2142b6c
Martin Zoller <git@spam.zoller.tv>
2011-02-03 22:12:19 -0500

    Fixed ugly bugs

--- db/migrate/20110203033104_adapt_foreign_keys.rb
+++ db/migrate/20110203033104_adapt_foreign_keys.rb
@@ -1,11 +1,11 @@
 class AdaptForeignKeys < ActiveRecord::Migration
   def self.up
-    rename_column :teams, :competition_group_id, :bill_id
-    rename_column :payments, :competition_group_id, :bill_id
+    rename_column :teams, :payment_group_id, :bill_id
+    rename_column :payments, :payment_group_id, :bill_id
   end
 
   def self.down
-    rename_column :teams, :bill_id, :competition_group_id
-    rename_column :payments, :bill_id, :competition_group_id 
+    rename_column :teams, :bill_id, :payment_group_id
+    rename_column :payments, :bill_id, :payment_group_id 
   end
 end
