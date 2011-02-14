531bb318006091c553702c742bb06f74207ff29c
Martin Zoller <git@spam.zoller.tv>
2011-02-14 13:31:49 -0500

    Removed secret token from repository

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OEventsNet::Application.config.secret_token = 'dd9ecc10eb61493946b9a11040c24ff90a5e9582d8db8bd1c0cbb9f80ca27c5803ec06366da93fcc39787999fbca72bb8eb06faa282455d24f8d22edf44fd5c4'
