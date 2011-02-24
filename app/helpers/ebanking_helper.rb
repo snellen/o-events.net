a7d53e8c89a393714d31c4842835306d6ff48d01
Silvan Nellen <silvan.nellen@gmail.com>
2011-02-24 18:25:35 -0500

    group reference number, implementation of check digit

--- app/helpers/ebanking_helper.rb
+++ app/helpers/ebanking_helper.rb
@@ -1,2 +1,21 @@
 module EbankingHelper
+    
+  # return full reference number (including final check digit)
+  def getFullReferenceNumber(number)
+    return groupRefNumber(number+checkDigit(number))
+  end
+  
+  def groupRefNumber(number)
+    number.reverse().scan(/.{1,5}/).join(' ').reverse()
+  end
+  
+  private
+  def checkDigit(number)
+    table = [0, 9, 4, 6, 8, 2, 7, 1, 3, 5]
+    carry = 0
+    for digit in number.each_char do
+      carry = table[(carry+Integer(digit))%10]
+    end
+    ((10-carry)%10).to_s
+  end
 end
