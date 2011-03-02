bd5715f09df5eb575dc85abc83e83f1d06f78e1e
Silvan Nellen <silvan.nellen@gmail.com>
2011-03-01 22:25:23 -0500

    moved login form to partial, use same partial for login box and login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,16 @@
 class SessionsController < ApplicationController
   skip_before_filter :authorize
-
+ 
   def new
+    if alreadyLoggedIn
+      redirect_to events_index_url
+    end
   end
 
   def create
+    if alreadyLoggedIn
+      redirect_to events_index_url
+    end
     if user = User.authenticate(params[:identifier], params[:password])
       session[:user_id] = user.id
       redirect_to events_index_url
@@ -18,4 +24,8 @@ class SessionsController < ApplicationController
     redirect_to events_index_url, :notice => noticeInfo("Logged out")
   end
 
+  private
+  def alreadyLoggedIn
+    User.find_by_id(session[:user_id]) != nil
+  end
 end
